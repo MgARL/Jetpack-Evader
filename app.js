e60e792897752297937a3ef1e5cdc1c6bc90a9c4
Miguel(MgARL) <mgarlcr@gmail.com>
2022-01-24 18:34:16 -0500

    added SoundFX to HomePage

--- app.js
+++ app.js
@@ -9,18 +9,38 @@
 
 let creditsBtn = document.querySelector("#creditsBtn");
 
-let closeCrBtn = document.querySelector('#close-modal-cr')
+let closeCrBtn = document.querySelector('#close-modal-cr');
+
+let playGameBtn = document.querySelector('#play-game-btn');
 
 //getting Modal
 
 let creditsModal= document.querySelector('#modal-credits');
 
+// creating Sound FX
+
+let playSoundFX = new Audio('/assets/music/magic.ogg');
+playSoundFX.volume = 0.5;
+
+let creditsClickSFX = new Audio('/assets/music/bling.ogg');
+creditsClickSFX.volume = 0.2;
+
 
 //adding event listener to show and hide with Credit
 creditsBtn.addEventListener('click', () => {
     creditsModal.classList.remove('d-none');
+    creditsClickSFX.play()
  });
  
  closeCrBtn.addEventListener('click', () => {
      creditsModal.classList.add('d-none'); 
+     creditsClickSFX.play()
  });
+
+ playGameBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    playSoundFX.play();
+    setTimeout(() => {
+        window.location.href = "/game.html"
+    },700)
+ })
